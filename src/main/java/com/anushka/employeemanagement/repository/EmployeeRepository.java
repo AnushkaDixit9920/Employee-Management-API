3b9932020d2c38fcb372321d4c0cd7482fd7d947
Anushka Dixit <anushka036btcseai24@igdtuw.ac.in>
2025-11-16 20:29:40 +0530

    Add files via upload

---
+++ src/main/java/com/anushka/employeemanagement/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.anushka.employeemanagement.repository;
+
+import com.anushka.employeemanagement.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
